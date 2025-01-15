d3931356b6f33c97fa88e1a9f4dace08fdfaf853
Ayomide Lawal <ayomidelawal700@gmail.com>
2025-01-15 10:59:09 +0100

    set up db connection

--- cmd/api/api.go
+++ cmd/api/api.go
@@ -17,6 +17,14 @@ type application struct {
 
 type config struct {
 	addr string
+	db   dbConfig
+}
+
+type dbConfig struct {
+	dsn          string
+	maxOpenConns int
+	maxIdleConns int
+	maxIdleTime  string
 }
 
 func (app *application) mount() *chi.Mux {
--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 
+	"github.com/devphaseX/mingle.git/internal/db"
 	"github.com/devphaseX/mingle.git/internal/env"
 	"github.com/devphaseX/mingle.git/internal/store"
 
@@ -12,10 +13,21 @@ import (
 func main() {
 	cfg := config{
 		addr: env.GetString("ADDR", ":8080"),
+		db: dbConfig{
+			dsn:          env.GetString("DB_ADDR", "postgres://mingle:adminpassword@localhost/mingle?sslmode=disable"),
+			maxOpenConns: env.GetInt("DB_MAX_OPEN_CONNS", 30),
+			maxIdleConns: env.GetInt("DB_MAX_IDLE_CONNS", 30),
+			maxIdleTime:  env.GetString("DB_MAX_IDLE_TIME", "15m"),
+		},
 	}
 
-	store := store.NewPostgressStorage(nil)
+	db, err := db.New(cfg.db.dsn, cfg.db.maxOpenConns, cfg.db.maxIdleConns, cfg.db.maxIdleTime)
 
+	if err != nil {
+		log.Panic(err)
+	}
+
+	store := store.NewPostgressStorage(db)
 	app := &application{
 		config: cfg,
 		store:  store,
