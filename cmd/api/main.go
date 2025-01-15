c2c4e5aaf619121652760c1a3cfa07f440dd97d0
Ayomide Lawal <ayomidelawal700@gmail.com>
2025-01-15 08:11:50 +0100

    add build tools co config

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,1 +1,20 @@
 package main
+
+import (
+	"log"
+
+	"github.com/devphaseX/mingle.git/internal/env"
+)
+
+func main() {
+	cfg := config{
+		addr: env.GetString("ADDR", ":8080"),
+	}
+
+	app := &application{
+		config: cfg,
+	}
+
+	mux := app.mount()
+	log.Fatal(app.serve(mux))
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/devphaseX/mingle.git
 
 go 1.23.2
+
+require github.com/go-chi/chi/v5 v5.2.0
